d68ecdb415f7c501c801444e44a7333aea927e4e
Shakir-Ahammed <shuvocsevu231@gmail.com>
2023-03-25 16:13:47 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('home', 'HomeController@index')->name('home');
+Route::get('about', 'HomeController@about')->name('about');
